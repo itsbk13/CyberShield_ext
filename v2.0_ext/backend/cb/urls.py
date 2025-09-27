57b0a0e83f884c970cb212a676144f01ba428b17
itsbk13 <barathsrini13@gmail.com>
2025-09-27 18:35:06 +0530

    Hided Credentials

--- v2.0_ext/backend/cb/urls.py
+++ v2.0_ext/backend/cb/urls.py
@@ -1,8 +1,8 @@
-
-
 from django.urls import path
 from .views import analyze_text
+from .views import get_mistral_key
 
 urlpatterns = [
     path('analyze/', analyze_text, name='analyze-text'),
+    path('analyze/api/get-mistral-key/', get_mistral_key, name='get_mistral_key'),
 ]
